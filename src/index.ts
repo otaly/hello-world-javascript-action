13961b79649f39ead2c80320bb04a62d3505477f
otaly <60144056+otaly@users.noreply.github.com>
2024-07-31 18:20:46 +0900

    biome追加

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,11 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const time = new Date().toTimeString();
+  core.setOutput('time', time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+  if (error instanceof Error) core.setFailed(error.message);
 }
