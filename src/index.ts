39ded21344d4b48ecf5a107eddb14eccf2bd0388
otaly <60144056+otaly@users.noreply.github.com>
2024-08-01 11:06:29 +0900

    タグの実験

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as github from '@actions/github';
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!!`);
+  console.log(`Hello ${nameToGreet}!!!`);
   const time = new Date().toTimeString();
   core.setOutput('time', time);
   // Get the JSON webhook payload for the event that triggered the workflow
